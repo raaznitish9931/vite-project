16093aaa2ff4d1207527d04f46dbd5807e8dc204
Nitish <raaznitish9931.com>
2024-09-16 14:09:31 +0530

    Intro

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
+// const App = () => {
+//   return (
+//     <div className="mt-5 w-[80%] m-auto">
+//       <h1 className="text-5xl flex items-center justify-center ">Durgesh mc</h1>
+//       {/* <h2 className="text-3xl">{import.meta.env.VITE_API_KEY}</h2> */}
+//       {/* <button className="px-5 py-2 bg-red-300 rounded ">Explore</button> */}
+//       <img   className="   w-80 flex items-center justify-center" src="https://m.media-amazon.com/images/M/MV5BYzVmZTgyODctNDBiZC00YzM2LWExYzAtODQzYjBkMjI5NmY3XkEyXkFqcGc@._V1_.jpg" alt="" />
+//     </div>
+
+
+//   );
+// };
+
+// export default App;
+
+
+
+
 const App = () => {
   return (
-    <div className="mt-5 w-[80%] m-auto">
-      <h1 className="text-5xl">This is React Vite Boileapate</h1>
-      <h2 className="text-3xl">{import.meta.env.VITE_API_KEY}</h2>
-      <button className="px-5 py-2 bg-red-300 rounded ">Explore</button>
-    </div>
-  );
-};
+    <div
+      className="  Box bg-black h-full w-full" >  <h1 className="text-8xl text-violet-600  font-bold flex justify-center mt-60">...WELCOME...🙋‍♂️</h1></div>
+
+  )
+}
 
 export default App;
